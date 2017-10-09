d8c9c53bb2f89474f294944f00a6b2ae9c4f6a73
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2017-10-08 22:35:16 -0500

    Add in test for testing map reads singleend.

---
+++ workflow/tests/test_map_reads.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import pytest
+import os
+
+
+def test_map_reads_singleend():
+    aligned_reads_report = os.path.dirname(os.path.abspath(__file__)) + \
+                '../output/alignReads/ENCFF646LXU.srt.bam.flagstat.qc'
+    samtools_report = open(aligned_reads_report).readlines()
+    assert '80795025' in samtools_report[1]
+    assert '80050072' in samtools_report[5]
+
+
+def test_map_reads_pairedend():
+    # Do the same thing for paired end data
+    pass
