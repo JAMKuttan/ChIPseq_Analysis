d6a5bea01c74b36d71b0f16c3109b27cf95d2088
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2017-10-10 13:04:13 -0500

    Update triming to have generalized paths.

--- workflow/tests/test_trim_reads.py
+++ workflow/tests/test_trim_reads.py
@@ -3,14 +3,18 @@
 import pytest
 import os
 
+test_data_path = os.path.dirname(os.path.abspath(__file__)) + \
+                '/../../test_data/'
+
+test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
+                '/../output/trimReads/'
+
 
 def test_trim_reads_singleend():
-    raw_fastq = os.path.dirname(os.path.abspath(__file__)) + \
-                '/../../test_data/ENCFF833BLU.fastq.gz'
-    trimmed_fastq = os.path.dirname(os.path.abspath(__file__)) + \
-                '/../output/trimReads/ENCFF833BLU_trimmed.fq.gz'
-    trimmed_fastq_report = os.path.dirname(os.path.abspath(__file__)) + \
-                '/../output/trimReads/ENCFF833BLU.fastq.gz_trimming_report.txt'
+    raw_fastq = test_data_path + 'ENCFF833BLU.fastq.gz'
+    trimmed_fastq = test_output_path + 'ENCFF833BLU_trimmed.fq.gz'
+    trimmed_fastq_report = test_output_path + \
+                            'ENCFF833BLU.fastq.gz_trimming_report.txt'
     assert os.path.getsize(raw_fastq) != os.path.getsize(trimmed_fastq)
     assert os.path.getsize(trimmed_fastq) == 2512853101
     assert 'Trimming mode: single-end' in open(trimmed_fastq_report).readlines()[4]
