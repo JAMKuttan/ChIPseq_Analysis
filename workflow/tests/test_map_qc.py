f4b48e0b8ab1fc4fbbb684406bd47f278963fcee
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2017-10-11 06:56:34 -0500

    Fix test.

--- workflow/tests/test_map_qc.py
+++ workflow/tests/test_map_qc.py
@@ -14,11 +14,11 @@ def test_map_qc_singleend():
     filtered_reads_report = test_output_path + 'ENCFF646LXU.filt.nodup.flagstat.qc'
     samtools_report = open(filtered_reads_report).readlines()
     assert '64962570 + 0 in total' in samtools_report[0]
-    assert '64962570 + 0 mapped (100% : N/A)' in samtools_report[4]
+    assert '64962570 + 0 mapped (100.00%:N/A)' in samtools_report[4]
     library_complexity = test_output_path + 'ENCFF646LXU.filt.nodup.pbc.qc'
     df_library_complexity = pd.read_csv(library_complexity, sep='\t')
     assert  df_library_complexity["NRF"].iloc[0] == 0.926192
-    assert  df_library_complexity["PBC1"].iloc[0] == 0.926775 
+    assert  df_library_complexity["PBC1"].iloc[0] == 0.926775
     assert  df_library_complexity["PBC2"].iloc[0] == 13.706885
 
 
