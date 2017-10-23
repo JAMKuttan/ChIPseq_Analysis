853c4d18b3dcfa2407a8c0402b947819eac9fe1e
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2017-10-23 14:29:18 -0500

    fix typo in file name during test.

--- workflow/tests/test_convert_reads.py
+++ workflow/tests/test_convert_reads.py
@@ -8,8 +8,8 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 
 
 def test_convert_reads_singleend():
-    assert os.path.exists(os.path.join(test_output_path, 'ENCFF646LXU.tagAlign.gz'))
-    assert os.path.exists(os.path.join(test_output_path, 'ENCFF646LXU.bedse.gz'))
+    assert os.path.exists(os.path.join(test_output_path, 'ENCFF646LXU.filt.nodup.tagAlign.gz'))
+    assert os.path.exists(os.path.join(test_output_path, 'ENCFF646LXU.filt.nodup.bedse.gz'))
 
 
 def test_map_qc_pairedend():
