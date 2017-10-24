48fa7dbb9a8e54f7534d2983b46790bf357370f6
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2017-10-24 18:48:22 -0500

    Fix test fil.e

--- workflow/tests/test_xcor.py
+++ workflow/tests/test_xcor.py
@@ -10,7 +10,8 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 
 def test_convert_reads_singleend():
     assert os.path.exists(os.path.join(test_output_path, 'ENCFF833BLU.filt.nodup.tagAlign.15.tagAlign.gz.cc.plot.pdf'))
-    df_xcor = pd.read_csv("ENCFF833BLU.filt.nodup.tagAlign.15.tagAlign.gz.cc.qc", sep="\t", header=None)
+    qc_file = os.path.join(test_output_path,"ENCFF833BLU.filt.nodup.tagAlign.15.tagAlign.gz.cc.qc")
+    df_xcor = pd.read_csv(qc_file, sep="\t", header=None)
     assert df_xcor[2].iloc[0] == '195,215,230'
     assert df_xcor[8].iloc[0] == 1.024836
     assert df_xcor[9].iloc[0] == 1.266678
