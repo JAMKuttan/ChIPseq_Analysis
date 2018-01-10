566234152d5e992b50c20ac9d2564b450935b3e5
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2018-01-10 15:36:02 -0600

    Fix control file path.

--- workflow/scripts/pool_and_psuedoreplicate.py
+++ workflow/scripts/pool_and_psuedoreplicate.py
@@ -242,8 +242,9 @@ def main():
         # Check controls against cutoff_ratio
         # if so replace with pool_control
         # unless single control was used
-        path_to_pool_control = cwd + '/' + pool_control
+
         if not single_control:
+            path_to_pool_control = cwd + '/' + pool_control
             if control_df.values.max() > 1.2:
                 logger.info("Number of reads in controls differ by " +
                     " > factor of %f. Using pooled controls." % (cutoff_ratio))
@@ -258,6 +259,8 @@ def main():
                                     % row['replicate'])
                         design_new_df.loc[index, 'control_tag_align'] = \
                                                             path_to_pool_control
+        else:
+            path_to_pool_control =  pool_control
 
         # Add in pseudo replicates
         tmp_metadata = design_new_df.loc[0].copy()
