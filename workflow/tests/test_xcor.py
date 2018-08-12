7bbb0bbd038580ac38ebca92748cf3c1f1149ef6
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2018-08-11 20:15:09 -0500

    Add in test cross-correlation.

--- workflow/tests/test_xcor.py
+++ workflow/tests/test_xcor.py
@@ -20,5 +20,9 @@ def test_convert_reads_singleend():
 
 @pytest.mark.integration
 def test_map_qc_pairedend():
-    # Do the same thing for paired end data
-    pass
+    assert os.path.exists(os.path.join(test_output_path, 'ENCFF582IOZ_val_2ENCFF957SQS_val_1.filt.nodup.tagAlign.15.tagAlign.gz.cc.plot.pdf'))
+    qc_file = os.path.join(test_output_path,"ENCFF582IOZ_val_2ENCFF957SQS_val_1.filt.nodup.tagAlign.15.tagAlign.gz.cc.qc")
+    df_xcor = pd.read_csv(qc_file, sep="\t", header=None)
+    assert df_xcor[2].iloc[0] == '205,410,430'
+    assert df_xcor[8].iloc[0] == 1.060266
+    assert df_xcor[9].iloc[0] == 4.308793
