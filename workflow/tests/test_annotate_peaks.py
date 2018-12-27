eea9ab16a584f97918100d20133484159bfecc14
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2018-12-27 08:18:37 -0600

    Remove testing design string.

--- workflow/tests/test_annotate_peaks.py
+++ workflow/tests/test_annotate_peaks.py
@@ -9,13 +9,6 @@ import utils
 test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
                 '/../output/peakAnnotation/'
 
-DESIGN_STRING = """sample_id\tbam_reads\tbam_index\texperiment_id\tbiosample\tfactor\ttreatment\treplicate\tcontrol_id
-A_1\tA_1.bam\tA_1.bai\tA\tLiver\tH3K27ac\tNone\t1\tB_1
-A_2\tA_2.bam\tA_2.bai\tA\tLiver\tH3K27ac\tNone\t2\tB_2
-B_1\tB_1.bam\tB_1.bai\tB\tLiver\tH3K27ac\tNone\t1\tB_1
-B_2\tB_2.bam\tB_2.bai\tB\tLiver\tH3K27ac\tNone\t2\tB_2
-"""
-
 
 @pytest.mark.singleend
 def test_annotate_peaks_singleend():
