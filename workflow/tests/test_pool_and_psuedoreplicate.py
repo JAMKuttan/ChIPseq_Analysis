4379c348a5bbd4addc93f12848354c9f4c6ed418
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-16 08:19:02 -0500

    update argv arguments

--- workflow/tests/test_pool_and_psuedoreplicate.py
+++ workflow/tests/test_pool_and_psuedoreplicate.py
@@ -64,7 +64,7 @@ def test_check_controls_single(design_experiment_3):
 @pytest.mark.unit
 def test_single_rep(design_experiment_2):
     sys.argv.extend(['-d', design_experiment_2])
-    sys.argv.extend(['-p', true])
+    sys.argv.extend(['-p', 'true'])
     sys.argv.extend(['-c', 1.2])
     single_rep = pool_and_psuedoreplicate.main()
 
