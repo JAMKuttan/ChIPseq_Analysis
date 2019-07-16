d3e068272af03c41a9a9e784392bebad85c52f0f
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-16 09:02:28 -0500

    remove paired and cutoff argvs

--- workflow/tests/test_pool_and_psuedoreplicate.py
+++ workflow/tests/test_pool_and_psuedoreplicate.py
@@ -64,7 +64,6 @@ def test_check_controls_single(design_experiment_3):
 @pytest.mark.unit
 def test_single_rep(design_experiment_2):
     sys.argv.extend(['--design', 'design_experiment_2'])
-    sys.argv.extend(['--paired', 'true'])
     single_rep = pool_and_psuedoreplicate.main()
 
 
